7b941bffa752d6c1db6f4100e8b0a6ccb1e2724f
Ninja-kitten <tran260@mail.chapman.edu>
2016-11-10 20:55:47 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -27,7 +27,7 @@ public:
     bool operator!=(Student s);
     bool operator<(Student s);
     bool operator>(Student s);
-    void operator<<(Student s);
+    void operator<<(ostream& os, Student s);
 
 private:
     unsigned int advisor;
