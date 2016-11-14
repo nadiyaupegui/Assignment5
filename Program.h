8c8c344d02af86ecf0831d7fcecb17f8692ffc95
Ninja-kitten <tran260@mail.chapman.edu>
2016-11-13 17:43:17 -0800

    Update Program.h

--- Program.h
+++ Program.h
@@ -1,1 +1,13 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+#include "GenBST.h"
+#include "GenStack.h"
+#include "Person.h"
+#include "Student.h"
+#include "Faculty.h"
+#include <string>
+using namespace std;
 
+
+
+#endif
