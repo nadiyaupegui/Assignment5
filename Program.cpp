500bea7d648572522bd455fd1858ae152fd87e78
Ninja-kitten <tran260@mail.chapman.edu>
2016-11-13 18:08:00 -0800

    Create Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,25 @@
+Program::Program(){}
+
+Program::~Program(){}
+
+Program::printMenu(){
+cout<<"What would you like to do?"<<endl;
+cout<<"(1) \t Print Students"<<endl;
+cout<<"(2) \t Print Faculty"<<endl;
+cout<<"(3) \t Find Student"<<endl;
+cout<<"(4) \t Find Faculty"<<endl;
+cout<<"(5) \t Print Student's Advisor Info"<<endl;
+cout<<"(6) \t Print Faculty Advisee Info"<<endl;
+cout<<"(7) \t Add New Student"<<endl;
+cout<<"(8) \t Delete Student"<<endl;
+cout<<"(9) \t Add New Faculty"<<endl;
+cout<<"(10) \t Delete Faculty"<<endl;
+cout<<"(11) \t Change Sudent's advisor"<<endl;
+cout<<"(12) \t Remove an advisee from a faculty member"<<endl;
+cout<<"(13) \t Rollback"<<endl;
+cout<<"(14) \t Exit"
+}
+
+Program::UserInterface(int n){
+
+}
